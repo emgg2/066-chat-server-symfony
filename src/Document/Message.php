3bcd24821316b5ad9489d9d8177c2601c908f91e
Eva Garcia <emgg2222@gmail.com>
2023-04-24 17:56:43 +0200

    Create User and Message collections

---
+++ src/Document/Message.php
@@ -0,0 +1,168 @@
+<?php
+namespace App\Document;
+
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Document\User;
+
+/**
+ * @MongoDB\Document()
+ * @UniqueEntity("username")
+ * @UniqueEntity("email")
+ */
+
+// If you want the User object to be serialized to the session, you need to implement Serializable
+// https://symfony.com/doc/current/security/entity_provider.html#what-do-the-serialize-and-unserialize-methods-do
+class Message
+{
+
+    /**
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\ReferenceOne(targetDocument=User::class)
+     * @Assert\NotBlank()
+     */
+    protected $from;
+
+    /**
+     * @MongoDB\ReferenceOne (targetDocument=User::class)
+     * @Assert\NotBlank()
+     */
+    protected $to;
+
+    /**
+     * @MongoDB\Field(type="string")
+     * @Assert\NotBlank()
+     */
+    protected $message;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFrom()
+    {
+        return $this->from;
+    }
+
+    /**
+     * @param mixed $from
+     */
+    public function setFrom(User $from): void
+    {
+        $this->from = $from;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTo()
+    {
+        return $this->to;
+    }
+
+    /**
+     * @param mixed $to
+     */
+    public function setTo(User $to): void
+    {
+        $this->to = $to;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message): void
+    {
+        $this->message = $message;
+    }
+
+
+
+    public function getSalt()
+    {
+        return '';
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+
+    public function serialize()
+    {
+        return serialize(array(
+            $this->from,
+            $this->to,
+            $this->id,
+            $this->message
+        ));
+    }
+
+
+    public function unserialize($serialized)
+    {
+        $data = unserialize($serialized);
+
+        list(
+            $this->from,
+            $this->to,
+            $this->id,
+            $this->message
+            ) = $data;
+    }
+
+
+
+
+
+}
--- src/Document/User.php
+++ src/Document/User.php
@@ -9,17 +9,12 @@ use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
  * @MongoDB\Document()
- * @UniqueEntity("username")
- * @UniqueEntity("email")
  */
 
 // If you want the User object to be serialized to the session, you need to implement Serializable
 // https://symfony.com/doc/current/security/entity_provider.html#what-do-the-serialize-and-unserialize-methods-do
 class User implements AdvancedUserInterface
 {
-    const ROLE_DEFAULT = 'ROLE_USER';
-
-    const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
 
     /**
      * @MongoDB\Id(strategy="auto")
@@ -30,62 +25,32 @@ class User implements AdvancedUserInterface
      * @MongoDB\Field(type="string")
      * @Assert\NotBlank()
      */
-    protected $name;
+    protected $from;
 
     /**
      * @MongoDB\Field(type="string")
      * @Assert\NotBlank()
      */
-    protected $username;
+    protected $to;
 
     /**
      * @MongoDB\Field(type="string")
      * @Assert\NotBlank()
      */
-    protected $email;
+    protected $message;
 
-    /**
-     * @MongoDB\Field(type="boolean")
-     */
-    protected $enabled;
 
-    /**
-     * @MongoDB\Field(type="string")
-     */
-    protected $password;
 
-    /**
-     * @MongoDB\Field(type="collection")
-     */
-    protected $roles;
 
     /**
      * User constructor.
      */
     public function __construct()
     {
-        $this->enabled = true;
-        $this->roles = array();
+        $this->online = false;
     }
 
-    /**
-     *
-     * @param string $role
-     * @return $this
-     */
-    public function addRole($role)
-    {
-        $role = strtoupper($role);
-        if ($role === static::ROLE_DEFAULT) {
-            return $this;
-        }
 
-        if (!in_array($role, $this->roles, true)) {
-            $this->roles[] = $role;
-        }
-
-        return $this;
-    }
 
     /**
      * Get id
@@ -108,16 +73,6 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Get username
-     *
-     * @return string $username
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
      * Get password
      *
      * @return string $password
@@ -132,41 +87,11 @@ class User implements AdvancedUserInterface
      *
      * @return boolean $enabled
      */
-    public function isEnabled()
+    public function isOnline()
     {
-        return $this->enabled;
+        return $this->online;
     }
 
-    /**
-     * Get roles
-     *
-     * @return array $roles
-     */
-    public function getRoles()
-    {
-        $roles = $this->roles;
-
-        // we need to make sure to have at least one role
-        $roles[] = static::ROLE_DEFAULT;
-
-        return array_unique($roles);
-    }
-
-    public function hasRole($role)
-    {
-        return in_array(strtoupper($role), $this->getRoles(), true);
-    }
-
-
-    public function removeRole($role)
-    {
-        if (false !== $key = array_search(strtoupper($role), $this->roles, true)) {
-            unset($this->roles[$key]);
-            $this->roles = array_values($this->roles);
-        }
-
-        return $this;
-    }
 
     /**
      * Set name
@@ -181,18 +106,6 @@ class User implements AdvancedUserInterface
         return $this;
     }
 
-    /**
-     * Set username
-     *
-     * @param string $username
-     * @return self
-     */
-    public function setUsername($username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
 
     /**
      * Set email
@@ -226,26 +139,9 @@ class User implements AdvancedUserInterface
      * @param boolean $enabled
      * @return self
      */
-    public function setEnabled($enabled): self
+    public function setOnline($online): self
     {
-        $this->enabled = (bool) $enabled;
-
-        return $this;
-    }
-
-    /**
-     * Set roles
-     *
-     * @param array $roles
-     * @return self
-     */
-    public function setRoles(array $roles): self
-    {
-        $this->roles = array();
-
-        foreach ($roles as $role) {
-            $this->addRole($role);
-        }
+        $this->online = (bool) $online;
 
         return $this;
     }
@@ -276,33 +172,17 @@ class User implements AdvancedUserInterface
     }
 
 
-    public function isSuperAdmin()
-    {
-        return $this->hasRole('ROLE_SUPER_ADMIN');
-    }
-
-    public function setSuperAdmin($boolean)
-    {
-        if (true === $boolean) {
-            $this->addRole('ROLE_SUPER_ADMIN');
-        } else {
-            $this->removeRole('ROLE_SUPER_ADMIN');
-        }
-
-        return $this;
-    }
-
     public function serialize()
     {
         return serialize(array(
             $this->password,
-            $this->username,
-            $this->enabled,
+            $this->online,
             $this->id,
             $this->email,
             $this->name,
         ));
     }
+
 
     public function unserialize($serialized)
     {
@@ -310,19 +190,22 @@ class User implements AdvancedUserInterface
 
         list(
             $this->password,
-            $this->username,
-            $this->enabled,
+            $this->online,
             $this->id,
             $this->email,
             $this->name,
             ) = $data;
     }
 
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return (string) $this->getUsername();
+    public function toJsonSerialize() {
+        return serialize(array(
+            $this->online,
+            $this->id,
+            $this->email,
+            $this->name,
+        ));
     }
+
+
+
 }
