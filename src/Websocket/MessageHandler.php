36ef30bfb4e2faa05b28b42e62b4a91966dedbea
Eva Garcia <emgg2222@gmail.com>
2023-05-09 17:30:46 +0200

    Create Websocket server

---
+++ src/Websocket/MessageHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Websocket;
+
+
+use Ratchet\MessageComponentInterface;
+use Exception;
+use Ratchet\ConnectionInterface;
+use SplObjectStorage;
+
+class MessageHandler implements  MessageComponentInterface
+{
+    protected $connections;
+
+    public function __construct()
+    {
+        $this->connections = new SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->connections->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        foreach($this->connections as $connection)
+        {
+            if($connection === $from)
+            {
+                continue;
+            }
+
+            $connection->send($msg);
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->connections->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, Exception $e)
+    {
+        $this->connections->detach($conn);
+        $conn->close();
+    }
+
+}
